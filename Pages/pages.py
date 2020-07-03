021823d1005a07d73142bcd5f142b4bcc7db5b3f
NexInfinite <stormparardiseonline@gmail.com>
2020-07-03 19:26:44 +0100

    Added pages.py and pages.json

--- Pages/pages.py
+++ Pages/pages.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 # Before reading:
 # It is worth learning a little bit about how Cogs work and how discord.py works. If you have some basic knowledge; this should be useful for you.
 # It is also worthy noting that my comments are a little M A D so prepare yourself for some epic and cool comments.
-# OH! Its also worth nothing (yes it is worth it) that the way this works is by reading from the pages.json file.
+# OH! Its also worth nothing (yes it is worth it) that the way this works is by reading from the Pages.json file.
 
 
 # The definitions below are a snippet of my json_commands.py file. You can just add:
@@ -31,9 +31,9 @@ class Pages(commands.Cog, name="Pages"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="pages")
+    @commands.command(name="Pages")
     async def pages(self, ctx):
-        """Shoe me the pages!"""
+        """Shoe me the Pages!"""
         page_limit = 3  # This variable determines the length of each page! I bet you are happy i told you that!
         current_page = 1  # This variable determines the page it starts off on!!!!!!! Honestly, you'd be lost without me. I'd recommend not changing this because you will have to edit the initial_embed.
         data = open_json("pages.json")
@@ -71,6 +71,22 @@ class Pages(commands.Cog, name="Pages"):
                 except IndexError:
                     pass
             await msg.edit(embed=next_page)  # Send it then scoopidy loop loop, loopidy scoop scoop. Scoop de scoop de loop
+
+    # If you want to be really cool (https://www.youtube.com/watch?v=39k0mmjlo0M) you can add this!
+    # Syntax: !additem "This is the header" This is the description
+    @commands.command(name="additem")
+    async def add_item(self, ctx, name, *, description):
+        """Pages 2 electric 2"""
+        new_item = {
+            "name": name,
+            "description": description
+        }
+        data = open_json("pages.json")
+        data.append(new_item)
+        save_json(data, "pages.json")
+        embed = discord.Embed(color=discord.Color.dark_blue())
+        embed.set_author(name="Item has been added.")
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
