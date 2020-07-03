cbbbb0cb4686a88b2d64f1baec88d5714ac2853b
NexInfinite <stormparardiseonline@gmail.com>
2020-07-03 20:28:33 +0100

    2nd Keyboard Scripts

--- 2nd Keyboard Shortcuts/2ndKeyboard.lua
+++ 2nd Keyboard Shortcuts/2ndKeyboard.lua
@@ -5,7 +5,7 @@
 -- plug in your 2nd keyboard, load this script into LUAmacros, and press the triangle PLAY button.
 -- Then, press any key on that keyboard to assign logical name ('MACROS') to macro keyboard
 clear() --clear the console from last run
-local keyboardIdentifier = '772BDB1'
+local keyboardIdentifier = '0000AAA'
 
 
 
